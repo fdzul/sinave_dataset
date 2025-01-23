1f61f569734862a537c92a627e816c3537be8a27
fdzul <felipe.dzul.m@gmail.com>
2025-01-23 13:54:56 -0600

    This commit update the file

--- maps.R
+++ maps.R
@@ -1,8 +1,30 @@
 
 load("~/Library/CloudStorage/OneDrive-Personal/automatic_read_sinave/8.RData/den/den2008_2024.RData")
 
+###
+x <- den2008_2015 |>
+    dplyr::filter(ANO == 2008) |>
+    dplyr::filter(CVE_EDO == "12") |>
+    dplyr::select(-IDE_CAL, -IDE_CALLE1, 
+                  -IDE_CALLE2, -DES_LOC,
+                  -IDE_CP,
+                  -IDE_COL, -CVE_LOC, -DES_LOC) |>
+    dplyr::rename(DES_EDO = DES_EDO.x,
+                  DES_MPO = DES_MPO.x)
+
+write.csv(x,
+          file = "dengue_2008_guerrero.txt")
 
 
-
+##
+x <- den2016_2024 |>
+    dplyr::filter(ANO == 2024) |>
+    dplyr::filter(CVE_EDO_REP == "12") 
+    dplyr::select(-IDE_CAL, -IDE_CALLE1, 
+                  -IDE_CALLE2, -DES_LOC,
+                  -IDE_CP,
+                  -IDE_COL, -CVE_LOC, -DES_LOC) |>
+    dplyr::rename(DES_EDO = DES_EDO.x,
+                  DES_MPO = DES_MPO.x)
 
 
