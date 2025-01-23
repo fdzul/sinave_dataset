3234f1dd30078906d141bd978ac01f6c3f6ecdf6
fdzul <felipe.dzul.m@gmail.com>
2025-01-23 13:58:57 -0600

    This commit update the file

--- maps.R
+++ maps.R
@@ -17,14 +17,17 @@ write.csv(x,
 
 
 ##
-x <- den2016_2024 |>
+y <- den2016_2024 |>
     dplyr::filter(ANO == 2024) |>
-    dplyr::filter(CVE_EDO_REP == "12") 
-    dplyr::select(-IDE_CAL, -IDE_CALLE1, 
-                  -IDE_CALLE2, -DES_LOC,
+    dplyr::filter(CVE_EDO_REP == "12") |>
+    dplyr::select(-IDE_CAL, -NUM_EXT,-NUM_INT,
+                  -DES_LOC_RES,-CVE_LOC_RES,
+                  -CVE_LOC_REP, -DES_LOC_REP,
                   -IDE_CP,
-                  -IDE_COL, -CVE_LOC, -DES_LOC) |>
-    dplyr::rename(DES_EDO = DES_EDO.x,
-                  DES_MPO = DES_MPO.x)
+                  -IDE_COL) 
+
+
+write.csv(y,
+          file = "dengue_2024_guerrero.txt")
 
 
